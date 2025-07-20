830011d10d342a2ba8a6acdb6369bf91bf87dfae
Ezequiel Enrique De Arriba <ezequiel.e.arriba@gmail.com>
2025-07-20 15:32:40 -0300

    "iniciamos em javascript"
    git commit -m "iniciamos em javascript"
    git add .
    git add -m
    
    git add -u
    git commit -m "iniciamos em javascript"
    git push origin master
    git push

---
+++ script.js
@@ -0,0 +1,113 @@
+console.log("OK")
+
+const productos = [
+    {
+        id: "01",
+        imagen: "img/botellaMcLaren.png",
+        nombre: "Botella Termica McLaren F1 official",
+        precio: 30000
+    }
+    ,
+    {
+        id: "02",
+        imagen: "img/camistaMandaCorta.png",
+        nombre: "Camiseta Manga Corta McLaren F1 official",
+        precio: 50000
+    }
+    ,
+    {
+        id: "03",
+        imagen: "img/hoddie.png",
+        nombre: "Hoddie McLaren F1 official Negre",
+        precio: 90000
+    }
+    ,
+    {
+        id: "04",
+        imagen: "img/imanDeHeladeraMcLaren.webp",
+        nombre: "Imán de Heladera McLaren F1 official",
+        precio: 7500
+    }
+    ,
+    {
+        id: "05",
+        imagen: "img/legoMclaren.webp",
+        nombre: "LEGO McLaren F1 official",
+        precio: 180000
+    }
+    ,
+    {
+        id: "06",
+        imagen: "img/llaveroMcLaren.png",
+        nombre: "Llavero McLaren F1 official",
+        precio: 5000
+    }
+    ,
+    {
+        id: "07",
+        imagen: "img/tazaMcLarec.png",
+        nombre: "Taza McLaren F1 official",
+        precio: 20000
+    }
+    ,
+    {
+        id: "08",
+        imagen: "img/pinbadge Mclaren.webp",
+        nombre: "Pin Badge McLaren F1 official",
+        precio: 7500
+    }
+    ,
+    {
+        id: "09",
+        imagen: "img/goraMcLaren.png",
+        nombre: "Gora McLaren F1 official",
+        precio: 50000
+    }
+]
+
+/* funcion para oedenar los objetos */
+
+function compararProductosPorIdAscendente(a, b) {
+    if (a.id < b.id) {
+        return 1;
+    }
+    if (a.id > b.id) {
+        return -1;
+    }
+    return 0;
+}
+
+productos.sort(compararProductosPorIdAscendente);
+
+let carrito = [];
+
+/* Agregar los productos al HTML */
+
+agregarProduto();
+
+function agregarProduto() {
+    const divProductos = document.querySelector(".productos");
+
+    if (!divProductos) {
+        console.error("No se encontró el elemento con la clase 'productos'");
+        return;
+    }
+    
+    for (let i=0; i < productos.length; i++) {
+        const producto = productos[i];
+        divProductos.insertAdjacentHTML("afterbegin", 
+            `
+            <div class="producto">
+                    <img src="${producto.imagen}" alt="${producto.nombre}">
+                    <div class="producto-contenido">
+                        <h3>${producto.nombre}</h3>
+                        <p> ID: ${producto.id}</p>
+                        <p> $${producto.precio}</p>
+                    </div>
+                <button class="agregar-carrito" data-id="${producto.id}">Agregar al carrito</button>
+            </div>
+            `
+        );
+
+    }
+} 
